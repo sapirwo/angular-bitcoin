9c373fca47ecdb54595a24ccced7b7f4c81c16c4
sapirwo <68658782+sapirwo@users.noreply.github.com>
2021-01-15 14:52:32 +0200

    Delete src directory

--- src/app/models/bitcoin.model.ts
+++
@@ -1,3 +0,0 @@
-export interface Bitcoin {
-    "USD": { "15m": number, "last": number, "buy": number, "sell": number, "symbol": string }
-}
--- src/app/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-    id: string;
-    name: string;
-    coins: number;
-    usd: number,
-    moves: any
-}
